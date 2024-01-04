b7599ae699e683962bd4e615728b1098799be21b
Vasile Hij <vasile.hijs@gmail.com>
2024-01-04 18:22:53 +0200

    2023_10_2

--- py/23/day10.py
+++ py/23/day10.py
@@ -11,14 +11,10 @@ handle_data = 'lines'  # by default
 class SolveTheDay(SolverFunctions):    
     @classmethod
     def helper(cls, data):
-        return
-    
-    @classmethod
-    def level_1(cls, data):
         grid = list(data)
         seen = set()
         start_row, start_col = None, None
-        
+
         pipes = {
             '|': (East, West),
             '-': (South, North),
@@ -37,44 +33,81 @@ class SolveTheDay(SolverFunctions):
                 pass
 
         def get_starting_point(x, y):
-            res = []
+            direction = []
             for dx, dy in four_directions:
                 for dxx, dyy in pipes[grid[x + dx][y + dy]]:
-                    print(x, y, dx, dy, dxx, dyy)
                     if dx + dxx == 0 and dy + dyy == 0:
-                        print(dx, dy)
-                        res.append((dx, dy))
-            assert len(res) == 2
-            
+                        direction.append((dx, dy))
+            assert len(direction) == 2
+
             for char, moves in pipes.items():
-                if all(move in moves for move in res):
+                if all(move in moves for move in direction):
                     return char
-        
+
         def get_valid_moves(x, y):
             pipe_type = grid[x][y]
             return [(x + dx, y + dy) for dx, dy in pipes[pipe_type]]
-        
+
         grid[start_row] = grid[start_row].replace('S', get_starting_point(start_row, start_col))
 
         x, y = start_row, start_col
         queue = deque([(x, y)])
-        
-        while queue:
+
+        while queue:  # BFS
             x, y = queue.popleft()
-            
+
             if (x, y) in seen:
                 continue
             seen.add((x, y))
-        
+
             for row, col in get_valid_moves(x, y):
                 queue.append((row, col))
-        
-        return len(seen) // 2
-        
 
+        return grid, seen
+
+    @classmethod
+    def level_1(cls, data):
+        _, seen = cls.helper(data)
+        return len(seen) // 2
 
     @classmethod
     def level_2(cls, data):
-        data = cls.helper(data)
-        
-        return
+        grid, seen = cls.helper(data)
+
+        new_grid = ''
+        for row, line in enumerate(grid):
+            for col, elem in enumerate(line):
+                new_grid += '.' if (row, col) not in seen else elem
+            new_grid += '\n'
+
+        new_grid = new_grid.split('\n')
+
+        counter = 0
+        for elem in new_grid:
+            outside = True
+            start_F = None
+            for el in elem:
+                match el:
+                    case '.':
+                        if not outside:
+                            counter += 1
+                    case '|':
+                        outside = not outside
+                    case 'F':
+                        start_F = True
+                    case 'L':
+                        start_F = False
+                    case '-':
+                        assert start_F is not None
+                    case '7':
+                        assert start_F is not None
+                        if not start_F:
+                            outside = not outside
+                        start_F = None
+                    case 'J':
+                        assert start_F is not None
+                        if start_F:
+                            outside = not outside
+                        start_F = None
+
+        return counter
