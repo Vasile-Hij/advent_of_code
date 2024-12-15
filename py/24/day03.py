bbc1995444034affacb475a3849806995d93162c
Vasile Hij <vasile.hijs@gmail.com>
2024-12-15 23:36:29 +0200

    Day 3 part 2 fix bugs for input - previuous worked sample only

--- py/24/day03.py
+++ py/24/day03.py
@@ -2,23 +2,36 @@ from src.common.utils import SolverFunctions
 import re
 
 title = '--- Day 3: Mull It Over ---'
-parser_method = 'str_strip'
-custom_parser_method = False  # if True no parser method is required!!!
+parser_method = ''
+custom_parser_method = True  # if True no parser method is required!!!
 visual_handler_data = 'paragraph'  # by default
 
 
 class SolveTheDay(SolverFunctions):
     @classmethod
     def helper(cls, data):
-        return cls.concat(data)
+        return cls.str_strip(data)
 
     @classmethod
     def level_1(cls, data):
-        return cls.calculations(data[0])
+        return cls.calculations(data)
 
     @classmethod
     def level_2(cls, data):
-        return cls.calculations(cls.exclude(data[0]))
+        is_enabled = True
+        result = 0
+
+        matches = re.findall(r"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)", data)
+
+        for string in matches:
+            if string == "do()":
+                is_enabled = True
+            elif string == "don't()":
+                is_enabled = False
+            elif string.startswith("mul") and is_enabled:
+                result += cls._product(cls.integers(cls.numbers(string)[0]))
+
+        return result
 
     @classmethod
     def calculations(cls, strings):
@@ -27,7 +40,3 @@ class SolveTheDay(SolverFunctions):
     @classmethod
     def numbers(cls, strings):
         return re.findall(r'mul\(\d+,\d+\)', strings)
-
-    @classmethod
-    def exclude(cls, strings):
-        return re.sub(r"don't\(\).*?(do\(\)|$)", ' ', strings)
